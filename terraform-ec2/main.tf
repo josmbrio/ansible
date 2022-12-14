41af5c936fd88a06c6e5237ad166ea9d79725744
Jose Briones <josmbrio@gmail.com>
2022-12-14 11:07:39 -0500

    Compilation of files

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -158,18 +158,3 @@ resource "aws_instance" "myapp-server-two" {
   }
 }
 
-resource "aws_instance" "myapp-server-three" {
-  ami = data.aws_ami.latest-amazon-linux-image.id
-  instance_type = var.instance_type
-
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-  availability_zone = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
-
-  tags = {
-    Name = "prod-server"
-  }
-}
---
+++ terraform-ec2/terraform.tfvars
@@ -0,0 +1,9 @@
+vpc_cidr_block = "10.0.0.0/16"
+subnet_cidr_block = "10.0.10.0/24"
+env_prefix = "dev"
+avail_zone = "us-east-1a"
+my_ip = "0.0.0.0/0"
+instance_type = "t2.micro"
+public_key_location = "~/.ssh/id_rsa.pub"
+ssh_key_private = "~/.ssh/id_rsa"
+
